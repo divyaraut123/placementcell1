2e7707373c0bd714f93eef1ea167558f51ae7443
Divya Raut <127081902+divyaraut123@users.noreply.github.com>
2023-07-13 21:09:56 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}@cluster0.zygzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+
+mongoose.connect('mongodb+srv://rautdivya591:Divyaraut591@cluster0.mzp7pi5.mongodb.net/');
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+
+db.once('open',function(){
+    console.log("Connected to Database :: MongoDB")
+});
+
+module.exports = db;
